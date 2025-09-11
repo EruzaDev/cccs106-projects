8c786bc1bb4700257acc47bb1ce26c1ac4f115db
Azure Dev <cjaylavapie5@gmail.com>
2025-09-11 10:20:40 +0800

    Added AlertDialogs

--- week3_labs/src/main.py
+++ week3_labs/src/main.py
@@ -42,19 +42,59 @@ def main(page: ft.Page):
                             )
     
     async def login_click(e):
+        page.vertical_alignment = ft.MainAxisAlignment.END
         success = ft.AlertDialog(
+            icon=ft.Icon(name=ft.Icons.CHECK_CIRCLE, color=ft.Colors.GREEN),
             modal=True,
-            title = ft.Text("Login Successful"),
+            title = ft.Text("Login Successful", text_align=ft.TextAlign.CENTER),
             content= ft.Text(f'Welcome, [username]'), # add username
-            alignment=ft.alignment.center,
             actions = [
                 ft.TextButton(
                     text="OK", on_click = lambda e: page.close(success), 
                 )
             ]
         )
+        
+        failure = ft.AlertDialog(
+            icon = ft.Icon(name=ft.Icons.ERROR, color=ft.Colors.RED),
+            modal = True,
+            title = ft.Text("Login Failed", text_align=ft.TextAlign.CENTER),
+            content = ft.Text("Invalid username or password"),
+            actions = [
+                ft.TextButton(
+                    text="OK", on_click = lambda e: page.close(failure), 
+                )
+            ]
+        )
 
-    page.add(page_title, user_name, password, login_click())
+        invalid = ft.AlertDialog(
+            icon=ft.Icon(name=ft.Icons.INFO, color=ft.Colors.BLUE),
+            modal = True,
+            title = ft.Text("Input Error", text_align=ft.TextAlign.CENTER),
+            content = ft.Text("Please enter username of password"),
+            actions = [
+                ft.TextButton(
+                    text = "OK", on_click = lambda e: page.close(invalid)
+                )
+            ]
+        )
+
+        database_error  = ft.AlertDialog(
+            modal = True,
+            title = ft.Text("Database Error"),
+            content = ft.Text("An error occured while connecting to the database"), 
+            alignment = ft.alignment.center,
+            actions = [ft.TextButton(
+                text="OK", on_click = lambda e: page.close(database_error)
+            )]
+        )
+
+        
+        page.open(invalid)
+
+    login_button = ft.ElevatedButton(text="Login", width=100, icon=ft.Icons.LOGIN,on_click=login_click)
+
+    page.add(page_title, user_name, password, login_button)
 
     
 
